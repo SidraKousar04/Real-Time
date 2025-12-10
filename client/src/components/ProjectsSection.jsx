d6a7d712c06e68ad4f26e06ee58201a20cc7c8d1
SidraKousar04 <sidrakousar458@gmail.com>
2025-12-10 14:27:31 +0530

    Update API endpoint for fetching projects

--- client/src/components/ProjectsSection.jsx
+++ client/src/components/ProjectsSection.jsx
@@ -6,7 +6,7 @@ const ProjectsSection = () => {
   const [projects, setProjects] = useState([]);
 
   useEffect(() => {
-    API.get('/projects')
+    API.get('/api/projects')
       .then((res) => setProjects(res.data))
       .catch((err) => console.error('Error fetching projects:', err));
   }, []);
